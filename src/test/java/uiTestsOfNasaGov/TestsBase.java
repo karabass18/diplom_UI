ca9b347bec86c3345e9d5719b591b1f8ac573b72
Aleksei Dementev <karabass18@gmail.com>
2023-03-05 12:02:09 +0500

    added logs

--- src/test/java/uiTestsOfNasaGov/TestsBase.java
+++ src/test/java/uiTestsOfNasaGov/TestsBase.java
@@ -19,6 +19,7 @@ public class TestsBase {
         //Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://www.nasa.gov";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
     }
 
