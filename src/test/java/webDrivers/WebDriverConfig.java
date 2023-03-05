d4806499786991def4aa4872b6fd8063c866831e
Aleksei Dementev <karabass18@gmail.com>
2023-03-05 18:26:48 +0500

    added logs

--- src/test/java/webDrivers/WebDriverConfig.java
+++ src/test/java/webDrivers/WebDriverConfig.java
@@ -1,29 +1,10 @@
 package webDrivers;
 
-//public class WebDriverConfig {
-//}
-
 import org.aeonbits.owner.Config;
 
-//@Config.Sources({
-  //      "classpath:config/${env}.properties"
-//})
 
 public interface WebDriverConfig extends Config {
-    // @Key("baseUrl")
-    //@DefaultValue("https://www.bookcity.kz")
-    //String getBaseUrl();
 
-    @Key("browser")
-    @DefaultValue("CHROME")
-    String getBrowser();
-
-    // @Key("browserVersion")
-    //@DefaultValue("100.0")
-    //String getBrowserVersion();
-
-    // @Key("browserSize")
-    //String getBrowserSize();
 
     @Key("remoteUrl")
     String getRemoteURL();
