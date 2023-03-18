88fdae829d8ea06084ba316ed34796d3c889bf60
Aleksei Dementev <karabass18@gmail.com>
2023-03-18 15:20:50 +0500

    added media

--- src/test/java/uiTestsOfNasaGov/FooterTests.java
+++ src/test/java/uiTestsOfNasaGov/FooterTests.java
@@ -1,39 +1,22 @@
 package uiTestsOfNasaGov;
 
-import config.ContentConfig;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
-import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.FooterPage;
 
 public class FooterTests extends TestsBase {
 
-   // ClassLoader cLoader = FooterTests.class.getClassLoader();
     FooterPage footerPage = new FooterPage();
 
     @Severity(SeverityLevel.NORMAL)
     @DisplayName("Проверка Футера")
     @Test
-    public void shouldContainТextFromCsvFile() {
-
-        /*try (InputStream stream = cLoader.getResourceAsStream("footerContent.csv");
-             CSVReader reader = new CSVReader(new InputStreamReader(stream, StandardCharsets.UTF_8))) {
-
-            List<String[]> content = reader.readAll();*/
-      //  ContentConfig contentConfig = ConfigFactory.create(ContentConfig.class, System.getProperties());
+    public void shouldContainТextFromPropertiesFile() {
 
         footerPage
                 .checkFooterVisible()
                 .checkFooterContent(contentConfig.getFooterContent());
-
-
     }
-  /*  @Test
-    public void test1() {
-        ResourcesConfig config = ConfigFactory.create(ResourcesConfig.class, System.getProperties());
-        String[] arrCont = config.getArray();
-        System.out.println(arrCont[1]);
-    }*/
 }
--- src/test/java/uiTestsOfNasaGov/HeadMenuTests.java
+++ src/test/java/uiTestsOfNasaGov/HeadMenuTests.java
@@ -1,21 +1,16 @@
 package uiTestsOfNasaGov;
 
-import com.opencsv.CSVReader;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.HeadMenuPage;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 public class HeadMenuTests extends TestsBase {
 
-   // ClassLoader cLoader = HeadMenuTests.class.getClassLoader();
     HeadMenuPage headMenuPage = new HeadMenuPage();
+
     @Severity(SeverityLevel.NORMAL)
     @DisplayName("Проверка логотипа")
     @Test
@@ -29,36 +24,21 @@ public class HeadMenuTests extends TestsBase {
     @Severity(SeverityLevel.CRITICAL)
     @DisplayName("Проверка 1-й строки верхнего меню")
     @Test
-    public void shouldContainTextFromCsvFileInThe1Row()
-            //throws Exception
-     {
+    public void shouldContainTextFromPropertiesFileInThe1Row() {
 
-       // try (InputStream stream = cLoader.getResourceAsStream("headMenuContent.csv");
-         //    CSVReader reader = new CSVReader(new InputStreamReader(stream, StandardCharsets.UTF_8))) {
-
-       //     List<String[]> content = reader.readAll();
-            headMenuPage
-                    .check1RowOfHeadMenuVisible()
-                    .check1RowOfHeadMenuContent(contentConfig.getHeadMenu1RowContent());
-      //  }
+        headMenuPage
+                .check1RowOfHeadMenuVisible()
+                .check1RowOfHeadMenuContent(contentConfig.getHeadMenu1RowContent());
     }
 
     @Severity(SeverityLevel.NORMAL)
     @DisplayName("Проверка 2-й строки верхнего меню")
     @Test
-    public void shouldContainTextFromCsvFileInThe2Row()
-    //        throws Exception
-    {
+    public void shouldContainTextFromPropertiesFileInThe2Row() {
 
-       // try (InputStream stream = cLoader.getResourceAsStream("headMenuContent.csv");
-        //     CSVReader reader = new CSVReader(new InputStreamReader(stream, StandardCharsets.UTF_8))) {
-
-          //  List<String[]> content = reader.readAll();
-            headMenuPage
-                    .check2RowOfHeadMenuVisible()
-                    .check2RowOfHeadMenuContent(contentConfig.getHeadMenu2RowContent());
-       // }
-
+        headMenuPage
+                .check2RowOfHeadMenuVisible()
+                .check2RowOfHeadMenuContent(contentConfig.getHeadMenu2RowContent());
 
     }
 }
